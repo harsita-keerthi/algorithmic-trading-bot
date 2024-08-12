32d55ef0bc08162e025dee1eb9250ea2a1086dc7
harsita <131437586+harsita-keerthi@users.noreply.github.com>
2024-08-12 13:24:40 -0700

    backtest script, fixed data feed issues

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
 import backtrader as bt
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from strategies.moving_average import MovingAverageCrossStrategy
 from strategies.mean_reversion import MeanReversionStrategy
 
@@ -7,4 +14,17 @@ def run_backtest(strategy_class):
     cerebro.addstrategy(strategy_class)
 
     # load data
-    data = bt.feeds.PandasData
+    data = pd.read_csv('data/aapl_data.csv')
+    data.rename(columns={'timestamp': 'datetime'}, inplace=True)
+
+    data['datetime'] = pd.to_datetime(data['datetime'])
+    data.set_index('datetime', inplace=True)
+
+    bt_data=bt.feeds.PandasData(dataname=data)
+    
+    cerebro.adddata(bt_data)
+    cerebro.run()
+    cerebro.plot()
+
+if __name__ == '__main__':
+    run_backtest(MovingAverageCrossStrategy)
